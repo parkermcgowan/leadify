b8871a0942d392f724a16ab5e8e0ce55c0f7fac7
Parker McGowan <parkermcgowan@gmail.com>
2014-07-30 11:56:50 -0500

    hello world with array

---
+++ homework/helloworldarray.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @helloWorld = ('H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', "\n" );
+
+foreach my $character (@helloWorld)
+{
+    print $character;
+}
