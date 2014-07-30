4e72a90b8febeedec8b42465989766a292b30619
Parker McGowan <parkermcgowan@gmail.com>
2014-07-30 12:59:57 -0500

    white board feelings

---
+++ homework/whiteboardarray.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use 5.18.2;
+
+my @whiteBoardFeelings = ("Thankful", "Gracious", "Indifferent", "Disturbed");
+print "@whiteBoardFeelings\n";
+push @whiteBoardFeelings, "Disgruntled";
+print "@whiteBoardFeelings\n";
+pop @whiteBoardFeelings;
+print "@whiteBoardFeelings\n";
+unshift @whiteBoardFeelings, "Disgruntled";
+print "@whiteBoardFeelings\n";
